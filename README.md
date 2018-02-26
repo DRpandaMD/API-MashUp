b0dcd2359a3bb6a8a745848bb1709259c6b7aace
DRpandaMD <michael.zarate.ce@gmail.com>
2018-02-26 11:20:45 -0600

    made progress to handle api requests and stuff them into the database

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # API-MashUp
+Look in api-mash-up folder for an example of api usage using python.
+
+Key packages are
+* Flask
+* SQL Alchemy
+* JSONify
+
+I am using google maps geocode api and passing that information along into Foursquare to retrieve specific
+restaurant information based on food type and location.
--- api-mash-up/flask_app_main.py
+++ api-mash-up/flask_app_main.py
@@ -7,6 +7,8 @@ from flask import Flask, jsonify, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+import sys
+import codecs
 
 # setup flask and the database
 app = Flask(__name__)
@@ -16,8 +18,11 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 # ROUTES
-app.route('/restaurants', method=['GET', 'POST'])
+# we want to be able to get all restaurants from the database
+# AND we want to be able to provide arguments in the url to add items to the data base
+@app.route('/restaurants', method=['GET', 'POST'])
 def all_restaurants_handler():
     if request.method == 'GET':
         # We want to return all restaurants
@@ -25,7 +30,29 @@ def all_restaurants_handler():
         return jsonify(restaurants=[i.serialize for i in restaurants])
 
     elif request.method == 'POST':
-        
+        # make a new restaurant and put it in the data base
+        location = request.args.get('location', '')
+        mealType = request.args.get('mealType', '')
+        restaurant_info = findARestaurant(mealType, location)
+        if restaurant_info != "No Restaurants Found": # we found a restaurnt based on the arguments
+            restaurant = Restaurant(restaurant_name=(restaurant_info['name']).encode(),
+                                    restaurant_address=(restaurant_info['address']).encode())
+            # for the above function we need to cover the case where we get non standard characters .encode() force
+            # converts what ever is in the JSON object into unicode as the .encode() default is UTF-8
+            session.add(restaurant)
+            session.commit()
+            return jsonify(restaurant=restaurant.serialize)
+        else:
+            return jsonify({"Error": "No Restaurants found for " + mealType + "in " + location})
+
+
+# Here we want to make sure we can GET, UPDATE or DELETE a specific restaurant
+@app.route('/restaurants/<int:id>', methods=['GET', 'POST', 'DELETE'])
+def restaurant_handler(id):
+    restaurant = session.query(Restaurant).filter_by(id=id).one()
+    if request.method == 'GET':
+        # return the specific restaurant we just queried for
+        return jsonify(restaurant=restaurant.serialize)
 
 
 # Application Start Point
